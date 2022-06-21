319a33675a537f4ebf8464deb110942e16dea7ba
silverdewm <107937482+silverdewm@users.noreply.github.com>
2022-06-21 23:09:06 +0800

    Update st.py

--- st.py
+++ st.py
@@ -11,3 +11,16 @@ st.write("预测集大小：",len(df)-split)
 st.subheader('选择预测目标')
 type = st.selectbox('请选择预测目标：',('Close','Turnover (Lacs)'))
 st.line_chart(df[type])
+st.subheader('选择机器学习算法')
+genre = st.selectbox("请选择时间序列预测算法",
+     ('移动平均算法', '线性回归算法', '最近邻算法', 'AutoARIMA算法', 'LSTM算法'))
+if genre == '移动平均算法':
+    MovingAverage(df, type, split)
+elif genre == '线性回归算法':
+     LinearRegression(df, type, split)
+elif genre == '最近邻算法':
+     KNearestNeighbours(df, type, split)
+elif genre == 'AutoARIMA算法':
+    AutoARIMA(df, type, split)
+elif genre == 'LSTM算法':
+    LongShortTM(df, type, split)
