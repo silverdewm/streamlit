3f849a08f382575df193503fe9f160d255c83309
silverdewm <107937482+silverdewm@users.noreply.github.com>
2022-06-21 22:59:15 +0800

    Update st.py

--- st.py
+++ st.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+
 st.subheader('读入数据')
 df = pd.read_csv('NSE-TATAGLOBAL11.csv')
 st.dataframe(df)
 
-st.subheader('选择时间序列')
+st.subheader('2.选择时间序列')
 options = np.array(df['Date']).tolist()
 
 (start_time, end_time) = st.select_slider("请选择时间序列长度：",
@@ -15,12 +16,13 @@ options = np.array(df['Date']).tolist()
 st.write("时间序列开始时间:",end_time)
 st.write("时间序列结束时间:",start_time)
 
-
+#setting index as date
 df['Date'] = pd.to_datetime(df.Date, format = '%Y-%m-%d')
 df.index = df['Date']
 
 df = df[start_time:end_time]
 st.dataframe(df)
+
 
 number = st.number_input("请输入训练集所占比例：",min_value=0.5,max_value=0.9,value=0.8,step=0.1)
 split = int(number * len(df))
