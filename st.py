a1253268d84eb68201e5c72a4b1f04a2fc466e8f
silverdewm <107937482+silverdewm@users.noreply.github.com>
2022-06-21 22:57:34 +0800

    Update st.py

--- st.py
+++ st.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 df = pd.read_csv('NSE-TATAGLOBAL11.csv')
+
 st.subheader('选择时间序列')
 options = np.array(df['Date']).tolist()
 
@@ -12,9 +13,15 @@ options = np.array(df['Date']).tolist()
 st.write("时间序列开始时间:",end_time)
 st.write("时间序列结束时间:",start_time)
 
-#setting index as date
+
 df['Date'] = pd.to_datetime(df.Date, format = '%Y-%m-%d')
 df.index = df['Date']
 
 df = df[start_time:end_time]
 st.dataframe(df)
+
+number = st.number_input("请输入训练集所占比例：",min_value=0.5,max_value=0.9,value=0.8,step=0.1)
+split = int(number * len(df))
+st.write("选择的数据集大小：",len(df))
+st.write("训练集大小：",split)
+st.write("预测集大小：",len(df)-split)
