da130d1e0f0f87de55689ba283f0ab29683169a4
silverdewm <107937482+silverdewm@users.noreply.github.com>
2022-06-22 14:53:47 +0800

    Update st.py

--- st.py
+++ st.py
@@ -6,17 +6,6 @@ from MovingAverage import MovingAverage
 from KNearestNeighbours import KNearestNeighbours
 
 df = pd.read_csv('NSE-TATAGLOBAL11.csv')
-st.subheader('选择时间序列')
-options = np.array(df['Date']).tolist()
-(start_time, end_time) = st.select_slider("请选择时间序列长度：",
-     options = options,
-     value= ('2016-05-04','2014-06-27',),
- )
-st.write("时间序列开始时间:",end_time)
-st.write("时间序列结束时间:",start_time)
-df['Date'] = pd.to_datetime(df.Date, format = '%Y-%m-%d')
-df.index = df['Date']
-df = df[start_time:end_time]
 st.dataframe(df)
 number = st.number_input("请输入训练集所占比例：",min_value=0.5,max_value=0.9,value=0.8,step=0.1)
 split = int(number * len(df))
